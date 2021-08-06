353a040e6a5c7966c8452ea72c2a1435168fd38b
laryangl <88349235+laryangl@users.noreply.github.com>
2021-08-06 16:36:15 +0300

    #1

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -665,7 +665,7 @@ async def pmpermit_on(event):
 
 
 @catub.cat_cmd(
-    pattern="(a|approve)(?:\s|$)([\s\S]*)",
+    pattern="(a|سماح)(?:\s|$)([\s\S]*)",
     command=("approve", plugin_category),
     info={
         "header": "To approve user to direct message you.",
@@ -741,7 +741,7 @@ async def approve_p_m(event):  # sourcery no-metrics
 
 
 @catub.cat_cmd(
-    pattern="(da|disapprove)(?:\s|$)([\s\S]*)",
+    pattern="(da|رفض)(?:\s|$)([\s\S]*)",
     command=("disapprove", plugin_category),
     info={
         "header": "To disapprove user to direct message you.",
@@ -792,7 +792,7 @@ async def disapprove_p_m(event):
 
 
 @catub.cat_cmd(
-    pattern="block(?:\s|$)([\s\S]*)",
+    pattern="بلوك(?:\s|$)([\s\S]*)",
     command=("block", plugin_category),
     info={
         "header": "To block user to direct message you.",
@@ -848,7 +848,7 @@ async def block_p_m(event):
 
 
 @catub.cat_cmd(
-    pattern="unblock(?:\s|$)([\s\S]*)",
+    pattern="رفع الحظر(?:\s|$)([\s\S]*)",
     command=("unblock", plugin_category),
     info={
         "header": "To unblock a user.",
@@ -881,7 +881,7 @@ async def unblock_pm(event):
 
 
 @catub.cat_cmd(
-    pattern="listapproved$",
+    pattern="الحمايه$",
     command=("listapproved", plugin_category),
     info={
         "header": "To see list of approved users.",
